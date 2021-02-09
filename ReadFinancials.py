27ba6c6814cb7ddb41bc76b4407ea479e80452df
Bharani Deepak <69731523+deepakbharani@users.noreply.github.com>
2021-02-09 18:57:58 +0100

    Update ReadFinancials.py

--- ReadFinancials.py
+++ ReadFinancials.py
@@ -1,4 +1,19 @@
-with open("balance_sheet.txt") as bs:
+"""
+Author: Bharani Deepak
+Info  : This script reads the financial data  from the folder "inputs"
+"""
+
+import os
+
+source_path = os.path.dirname(__file__)
+source_path = source_path.replace('/','\\')
+
+# files inside "input" folder is defined in tuple
+input_data = ('balance_sheet.txt','cash_flow.txt','income_statement.txt')
+
+balance_sheet = os.path.join(source_path,'inputs\\',input_data[0])
+print(balance_sheet)
+
+with open(balance_sheet,'r') as bs:
     for l in bs:
         print(l.strip())
-
