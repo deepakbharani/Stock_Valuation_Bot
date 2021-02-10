639b0753be1ca1fb300f5d8773770103de538c14
Bharani Deepak <69731523+deepakbharani@users.noreply.github.com>
2021-02-10 22:14:16 +0100

    Update Base.py

--- Base.py
+++ Base.py
@@ -12,15 +12,17 @@ logger.addHandler(file_handler)
 
 class Base:
 
-    def __init__(self,input_data=[]):
+    def __init__(self,input_data=[],path=[]):
         self.indata = input_data
+        self.path = path
 
         if len(self.indata) is 3:
-            logger.info(" All 3 Financial statements have been accepted")
+            logger.info("All 3 Financial statements have been accepted")
+            self.readfinancials()
         else:
             logger.error("There must be 3 financial statements -> please check input folder")
 
-    def readfinancials(self,self.indata):
+    def readfinancials(self):
 
         # source_path is the path where this script is saved
         source_path = os.path.dirname(__file__)
@@ -28,9 +30,16 @@ class Base:
 
         logger.info("Creating list of paths for the files containing financial data")
         # Set path for all the financial files inside "inputs" folder
-        path = []
         for file in self.indata:
-            path.append(os.path.join(source_path, 'inputs\\', file))
+            self.path.append(os.path.join(source_path, 'inputs\\', file))
 
-        return path
+        with open(self.path[0], 'r') as bs:
+            balsheet = bs.readlines()
 
+        with open(self.path[1], 'r') as cf:
+            cashflow = cf.readlines()
+
+        with open(self.path[1], 'r') as ins:
+            incomestmt = ins.readlines()
+
+        return balsheet, cashflow,incomestmt
