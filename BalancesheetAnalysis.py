2a5fd13a3aae28e41a8bd5a911f1333228be19ec
Bharani Deepak <69731523+deepakbharani@users.noreply.github.com>
2021-02-16 21:08:03 +0100

    Update BalancesheetAnalysis.py

--- BalancesheetAnalysis.py
+++ BalancesheetAnalysis.py
@@ -1,6 +1,22 @@
+"""
+Author : Bharani Deepak
+Info   : This Subclass contains Methods and Attributes for analysing Balance Sheet and
+         it inherits from Base Class
+Purpose: Accesses the Balance Sheet and performs Balance Sheet analysis
+"""
 from Base import *
+import pandas as pd
 
 class BalancesheetAnalysis(Base):
 
     def __init__(self):
         super().__init__()
+
+    def currentratio(self):
+        logger.info("Calculating Current Ratio")
+        print(self.balsheet.loc['Current Assets'].dtypes)
+        # print(self.balsheet.loc[['Current Assets'],['9/29/2017']])
+        # print(self.balsheet.columns)
+
+        # growth = Base.percentage_growth(float(self.balsheet.loc[['Current Assets'],['9/29/2017']]),float(self.balsheet.loc[['Current Assets'],['9/29/2018']]))
+        # print(growth)
