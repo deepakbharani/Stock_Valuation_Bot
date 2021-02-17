5f3f022295b3af8f912d93bd800f41f59a38baf9
Bharani Deepak <69731523+deepakbharani@users.noreply.github.com>
2021-02-17 20:38:58 +0100

    Update Base.py

--- Base.py
+++ Base.py
@@ -46,19 +46,19 @@ class Base:
         with open(self.path[0], 'r') as bs:
             logger.info("Reading Balance sheet")
             data_bs = bs.readlines()
-            self.balsheet = self.listformatter(data_bs)
+            self.balsheet,self.bs_column_name = self.listformatter(data_bs)
             logger.info("Balance sheet read")
 
         with open(self.path[1], 'r') as cf:
             logger.info("Reading Cash flow statement")
             data_cf = cf.readlines()
-            self.cashflow = self.listformatter(data_cf)
+            self.cashflow,self.cs_column_name = self.listformatter(data_cf)
             logger.info("Cash flow statement read")
 
         with open(self.path[2], 'r') as ins:
             logger.info("Reading Income statement")
             data_ins = ins.readlines()
-            self.incomestmt = self.listformatter(data_ins)
+            self.incomestmt,self.ins_column_name = self.listformatter(data_ins)
             logger.info("Income statement read")
 
         return self.balsheet, self.cashflow, self.incomestmt
@@ -67,17 +67,29 @@ class Base:
 
         # This function formats the data from input file to meaningful elements of the list
         formatted_list = []
+
         if len(data[0]) is 46:
             data[0] = "Breakdown 9/29/2020 9/29/2019 9/29/2018 9/29/2017"
             column_name = list(data[0].split())
+            # Rearrange columns names for exchanging columns
+            cname = column_name.copy()
+            cname[1:len(cname)] = cname[1:len(cname)][::-1]
             num_years = 4
+
         elif len(data[0]) is 49:
             data[0] = "Breakdown TTM 9/29/2020 9/29/2019 9/29/2018 9/29/2017"
             column_name = list(data[0].split())
+            # Rearrange columns names for exchanging columns
+            cname = column_name.copy()
+            cname[1:len(cname)] = cname[1:len(cname)][::-1]
             num_years = 5
+
         else:
             num_years = 4
             column_name = ['Breakdown','2020','2019','2018','2017']
+            # Rearrange columns names for exchanging columns
+            cname = column_name.copy()
+            cname[1:len(cname)] = cname[1:len(cname)][::-1]
 
         for items in data:
             items = items.split()
@@ -89,10 +101,16 @@ class Base:
         dataframe = pd.DataFrame(formatted_list, columns = column_name)
         dataframe.drop(0,inplace = True)
         dataframe.set_index('Breakdown',inplace=True)
+        dataframe = dataframe[cname[1:len(cname)]]                      # Rearranging columns
 
-        return dataframe
+        return dataframe,column_name
 
     @classmethod
-    def percentage_growth(self,val1,val2):
+    def percentage_growth(self,list_data):
+
         # Calculate percentage change / growth for two value
-        return ((val2-val1)/val1) * 100
+        growth = []
+        for i in range(0,len(list_data)-1):
+            growth.append(((list_data[i+1]-list_data[i])/list_data[i]) * 100)
+
+        return growth
