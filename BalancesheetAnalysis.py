39a523f9b927639bf8a944aad31f5602780c0c92
Bharani Deepak <69731523+deepakbharani@users.noreply.github.com>
2021-02-19 20:04:49 +0100

    Update BalancesheetAnalysis.py

--- BalancesheetAnalysis.py
+++ BalancesheetAnalysis.py
@@ -4,7 +4,9 @@ Info   : This Subclass contains Methods and Attributes for analysing Balance She
          it inherits from Base Class
 Purpose: Accesses the Balance Sheet and performs Balance Sheet analysis
 """
+
 from Base import *
+from Plotter import *
 import pandas as pd
 
 logger = logging.getLogger(__name__)
@@ -12,7 +14,9 @@ file_handler = logging.FileHandler('logfile.log')
 file_handler.setLevel(logging.ERROR)
 logger.addHandler(file_handler)
 
-class BalancesheetAnalysis(Base):
+pt = Plotter()
+
+class BalancesheetAnalysis(Base,Plotter):
 
     def __init__(self):
         super().__init__()
@@ -24,5 +28,7 @@ class BalancesheetAnalysis(Base):
         cur_asset = np.array(self.balsheet.loc['Current Assets']).astype('int32')
         cur_liability = np.array(self.balsheet.loc['Current Liabilities']).astype('int32')
         cur_ratio = cur_asset/cur_liability
-
+        pt.twoDplot('Current Ratio', 'Years', 'Current Ratio', 'Current Ratio past few Years', cur_ratio,
+                    self.bs_column_name[1:])
+        pt.twoDplot('Growth in Current Ratio', 'Years', 'Growth', 'Current Ratio growth', Base.percentage_growth(cur_ratio))
         return Base.percentage_growth(cur_ratio)
