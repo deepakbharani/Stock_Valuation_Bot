0d0a25f8ccf2e3cb1115b4605a24aa783cc43bef
Bharani Deepak <69731523+deepakbharani@users.noreply.github.com>
2021-02-19 20:06:16 +0100

    Add files via upload

---
+++ Plotter.py
@@ -0,0 +1,35 @@
+"""
+Author : Bharani Deepak
+Info   : This Subclass contains Methods and Attributes for plotting the results
+Purpose: plot
+"""
+
+from BalancesheetAnalysis import *
+from matplotlib import pyplot as plt
+
+plt.xkcd()
+
+class Plotter():
+
+    def __init__(self):
+        pass
+
+    def twoDplot(self,labeltext,xlabel,ylabel,title,array,xaxis = None):
+
+        if xaxis is None:
+            xaxis = np.arange(1,len(array)+1)
+        else:
+            xaxis = xaxis
+
+        plt.plot(xaxis,array,label = 'labeltext', marker = 'o')
+        plt.title(title)
+        plt.xlabel(xlabel)
+        plt.ylabel(ylabel)
+        plt.grid(True)
+        plt.show()
+
+
+        print(xaxis)
+#        plot()
+
+        pass
