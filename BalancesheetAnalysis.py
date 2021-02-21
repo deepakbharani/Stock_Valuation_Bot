589d6d13c9390391fbf8597428437f1216a8cb59
Bharani Deepak <69731523+deepakbharani@users.noreply.github.com>
2021-02-21 19:19:01 +0100

    Update BalancesheetAnalysis.py

--- BalancesheetAnalysis.py
+++ BalancesheetAnalysis.py
@@ -1,117 +1,103 @@
 """
 Author : Bharani Deepak
-Info   : This Base Class contains common Methods and Attributes
-Purpose: Reads the financial statements and returns dataframes containing the data
+Info   : This Subclass contains Methods and Attributes for analysing and calculating
+         Liquidity Ratios
+Helper : Plot arguments (labeltext,xlabel,ylabel,title,array,xaxis = None):
 """
 
-import os
-import logging
-import pandas as pd
-import numpy as np
+from Base import *
+from Plotter import *
 
 logger = logging.getLogger(__name__)
 file_handler = logging.FileHandler('logfile.log')
 file_handler.setLevel(logging.ERROR)
 logger.addHandler(file_handler)
 
-class Base:
+pt = Plotter()
 
-    def __init__(self,path = None):
+class LiquidityRatio(Base,Plotter):
 
-        # files inside "input" folder is defined in tuple
-        self.indata = ('balance_sheet.txt', 'cash_flow.txt', 'income_statement.txt')
+    def __init__(self):
+        super().__init__()
+        self.G_cur_ratio = self.currentratio()                      # Growth in current ratio
+        self.G_debtequity = self.debttoequity()                     # Growth in Debt to Equity
+        self.G_cashratio = self.cashratio()                         # Growth in Cash Ratio
+        self.G_oper_cashflow_ratio = self.oper_cashflow_ratio()     # Growth in Operating Cashflow
 
-        if path == None:
-            self.path = []
-        else:
-            self.path = path
 
+    def currentratio(self):
 
-        if len(self.indata) is 3:
-            logger.info("All 3 Financial statements have been accepted")
-            self.balsheet,self.cashflow,self.incomestmt = self.readfinancials()
-        else:
-            logger.error("There must be 3 financial statements -> please check input folder")
+        """
+        Current Ratio:
+        The ability of a company to pay off its current liabilities
+        ***HIGHER the BETTER***
+        """
 
-    def readfinancials(self):
+        logger.info("Calculating Current Ratio and its growth")
+        self.cur_asset = np.array(self.balsheet.loc['Current Assets']).astype('float')
+        self.cur_liability = np.array(self.balsheet.loc['Current Liabilities']).astype('float')
+        self.cur_ratio = np.divide(self.cur_asset,self.cur_liability)
+        self.A_cur_ratio = self.cur_ratio.mean()
 
-        # source_path is the path where this script is saved
-        self.source_path = os.path.dirname(__file__)
-        self.source_path = self.source_path.replace('/', '\\')
+        ## PLOTTING
+        # pt.twoDplot('Current Asset / Current Liability', 'Years', 'Current Ratio', 'Current Ratio past few Years', self.cur_ratio,
+        #             self.bs_column_name[1:])
+        # pt.twoDplot('Growth in Current Ratio', 'Years', 'Growth', 'Current Ratio growth', Base.percentage_growth(self.cur_ratio))
 
-        logger.info("Creating list of paths for the files containing financial data")
-        # Set path for all the financial files inside "inputs" folder
-        for file in self.indata:
-            self.path.append(os.path.join(self.source_path, 'inputs\\', file))
+        return Base.percentage_growth(self.cur_ratio)
 
-        with open(self.path[0], 'r') as bs:
-            logger.info("Reading Balance sheet")
-            data_bs = bs.readlines()
-            self.balsheet,self.bs_column_name = self.listformatter(data_bs)
-            logger.info("Balance sheet read")
+    def debttoequity(self):
 
-        with open(self.path[1], 'r') as cf:
-            logger.info("Reading Cash flow statement")
-            data_cf = cf.readlines()
-            self.cashflow,self.cs_column_name = self.listformatter(data_cf)
-            logger.info("Cash flow statement read")
+        """
+        Debt to Equity ratio:
+        Long term debt to its shareholders equity
+        ***LOWER the BETTER***
+        """
+        logger.info("Calculating Debt to Equity ratio")
+        self.long_term_debt = np.array(self.balsheet.loc['Long Term Debt']).astype('float')
+        self.shareholder_equity = np.array(self.balsheet.loc['Stockholders\' Equity']).astype('float')
+        self.debt2equity = np.divide(self.long_term_debt,self.shareholder_equity)
 
-        with open(self.path[2], 'r') as ins:
-            logger.info("Reading Income statement")
-            data_ins = ins.readlines()
-            self.incomestmt,self.ins_column_name = self.listformatter(data_ins)
-            logger.info("Income statement read")
+        ## PLOTTING
+        # pt.twoDplot('Debt to Equity', 'Years', 'Debt to Equity', 'Debt to Equity ratio', self.debt2equity,
+        #             self.bs_column_name[1:])
 
-        return self.balsheet, self.cashflow, self.incomestmt
+        return Base.percentage_growth(self.debt2equity)
 
-    def listformatter(self, data):
+    def cashratio(self):
 
-        # This function formats the data from input file to meaningful elements of the list
-        formatted_list = []
+        """
+        Cash Ratio:
+        Cash and Cash Equivalent to Current Liabilities
+        ***HIGHER the BETTER***
+        """
+        logger.info("Calculating Cash Ratio")
+        self.cash_cashequ = np.array(self.balsheet.loc['Cash And Cash Equivalents']).astype('float')
+        self.curr_liabilities = np.array(self.balsheet.loc['Current Liabilities']).astype('float')
+        self.cshratio = np.divide(self.cash_cashequ,self.cur_liability)
 
-        if len(data[0]) is 46:
-            data[0] = "Breakdown 9/29/2020 9/29/2019 9/29/2018 9/29/2017"
-            column_name = list(data[0].split())
-            # Rearrange columns names for exchanging columns
-            cname = column_name.copy()
-            cname[1:len(cname)] = cname[1:len(cname)][::-1]
-            num_years = 4
+        ## PLOTTING
+        # pt.twoDplot('Cash and Cash Equivalent to Current Liabilities', 'Years', 'Cash Ratio', 'Cash Ratio', self.cshratio,
+        #             self.bs_column_name[1:])
 
-        elif len(data[0]) is 49:
-            data[0] = "Breakdown TTM 9/29/2020 9/29/2019 9/29/2018 9/29/2017"
-            column_name = list(data[0].split())
-            # Rearrange columns names for exchanging columns
-            cname = column_name.copy()
-            cname[1:len(cname)] = cname[1:len(cname)][::-1]
-            num_years = 5
+        return Base.percentage_growth(self.cshratio)
 
-        else:
-            num_years = 4
-            column_name = ['Breakdown','2020','2019','2018','2017']
-            # Rearrange columns names for exchanging columns
-            cname = column_name.copy()
-            cname[1:len(cname)] = cname[1:len(cname)][::-1]
+    def oper_cashflow_ratio(self):
 
-        for items in data:
-            items = items.split()
-            items[0:(len(items) - num_years)] = [' '.join(items[0:(len(items) - num_years)])]
-            for elements in range(-num_years,0):
-                items[elements] = items[elements].replace(',','')
-            formatted_list.append(items)
+        """
+        Operative Cashflow Ratio:
+        The operating cash flow ratio is a measure of how readily current liabilities
+        are covered by the cash flows generated from a company's operations.
+        ***HIGHER the BETTER***
+        """
 
-        dataframe = pd.DataFrame(formatted_list, columns = column_name)
-        dataframe.drop(0,inplace = True)
-        dataframe.set_index('Breakdown',inplace=True)
-        dataframe = dataframe[cname[1:len(cname)]]                      # Rearranging columns
+        logger.info("Calculating Operating Cashflow ratio")
+        self.op_cashflow = np.array(self.cashflow.loc['Operating Cash Flow']).astype('float')
+        self.op_cashflow_ratio = np.divide(self.op_cashflow[0:-1],self.cur_liability)
+        
+        ## PLOTTING
+        # pt.twoDplot('Operating Cashflow to Current Liabilities', 'Years', 'Operating Cashflow ratio',
+        #             'Operating Cashflow ratio', self.op_cashflow_ratio,self.bs_column_name[1:])
+        # pt.twoDplot('Growth in Current Ratio', 'Years', 'Growth', 'Current Ratio growth', Base.percentage_growth(self.cur_ratio))
 
-        return dataframe,cname
-
-    @classmethod
-    def percentage_growth(self,vector):
-
-        # Calculate percentage change / growth for two value
-        growth = []
-        for i in range(0,len(vector)-1):
-            growth.append(((vector[i+1]-vector[i])/vector[i]) * 100)
-
-        return growth
+        return Base.percentage_growth(self.op_cashflow_ratio)
