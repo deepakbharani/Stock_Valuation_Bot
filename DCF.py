cffffb5ea92a51d7269b32e72091fa0a5971e423
deepakbharani <deepakbharani@gmail.com>
2021-03-03 21:14:20 +0100

    DCF Class added
    DCF class expanded with CAGR calculation of total revenue growth rate

--- DCF.py
+++ DCF.py
@@ -15,3 +15,10 @@ class DCF(ProfitabilityRatio):
         self.stock_ticker = 'AAPL'
         self.tic = yf.Ticker(self.stock_ticker)
         self.sharesoutstanding = self.tic.info.get('sharesOutstanding')
+        self.G_cagr_tot_rev_gr = self.cagr_tot_revenue_gr()
+
+    # Calculate CAGR of the growth rate of total_revenue
+    def cagr_tot_revenue_gr(self):
+        self.G_tot_revenue = np.array(Base.percentage_growth(self.tot_revenue))
+        return self.G_tot_revenue.mean()
+
